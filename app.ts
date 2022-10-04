8f05b3dc5bdf3a34281e8364385c15bb2c079c30
Salman Bukhari <salmanbukhari@NIS-PK662s-MacBook-Pro.local>
2022-10-04 23:14:27 +0500

    02-type-aliases done

--- app.ts
+++ app.ts
@@ -1,10 +1,17 @@
+type Combineable = number | string;
+type conversionType = 'as-text' | 'as-number';
+
 function add(n1: number, n2: number, showResult: boolean): number {
     if (showResult) {
         return n1 + n2;
     }
 }
 
-function combine(n1: number | string, n2: number | string, resultConversionType: 'as-text' | 'as-number'): number | string {
+function combine(
+    n1: Combineable, 
+    n2: Combineable, 
+    resultConversionType: conversionType): Combineable 
+{
     let result;
     if (typeof n1 === 'number' && typeof n2 === 'number' && resultConversionType === 'as-number') {
         result = n1 + n2;
