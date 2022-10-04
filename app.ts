726f855589eda77afb06fc0361d26e2cc7f4d636
Salman Bukhari <salmanbukhari@NIS-PK662s-MacBook-Pro.local>
2022-10-04 22:47:08 +0500

    02-unions example done

--- app.ts
+++ app.ts
@@ -4,6 +4,16 @@ function add(n1: number, n2: number, showResult: boolean): number {
     }
 }
 
+function combine(n1: number | string, n2: number | string): number | string {
+    let result;
+    if (typeof n1 === 'number' && typeof n2 === 'number') {
+        result = n1 + n2;
+    } else {
+        result = n1.toString() + n2.toString();
+    }
+    return result;
+} 
+
 const num1 = 2.3;
 const num2 = 2;
 const showResult = false;
