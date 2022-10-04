075e4c28e8eff1c7a596f56f0a09dd2abda75123
Salman Bukhari <salmanbukhari@NIS-PK662s-MacBook-Pro.local>
2022-10-04 23:46:44 +0500

    02-never-type return done

--- app.ts
+++ app.ts
@@ -7,3 +7,12 @@ userInput = "Salman";
 if (typeof userInput === 'string') {
     username = userInput;
 }
+
+function generateError (message: string, code: number): never {
+    throw {
+        message, errorCode: code
+    };
+}
+
+const result = generateError("An error occurred", 500);
+console.log(result);
