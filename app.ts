cb71b8cae09f4b60eded5d0314d3a23f7eb76a4b
Salman Bukhari <salmanbukhari@NIS-PK662s-MacBook-Pro.local>
2022-10-04 23:05:07 +0500

    02-literals done

--- app.ts
+++ app.ts
@@ -4,15 +4,18 @@ function add(n1: number, n2: number, showResult: boolean): number {
     }
 }
 
-function combine(n1: number | string, n2: number | string): number | string {
+function combine(n1: number | string, n2: number | string, resultConversionType: 'as-text' | 'as-number'): number | string {
     let result;
-    if (typeof n1 === 'number' && typeof n2 === 'number') {
+    if (typeof n1 === 'number' && typeof n2 === 'number' && resultConversionType === 'as-number') {
         result = n1 + n2;
     } else {
         result = n1.toString() + n2.toString();
-    }
+    } 
+
     return result;
 } 
+
+console.log(combine(2, 2, 'as-number'))
 
 const num1 = 2.3;
 const num2 = 2;
